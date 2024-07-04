03c9e0e67b0a9c5bb108ded8362302a8f99fc551
sarra.mannai <sarra.mannai@bertschinnovation.com>
2024-07-04 19:16:32 +0100

    add salle and interceptor for auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
- import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './register/register.component';
+import { MeetingRoomsComponent } from './meeting-rooms/meeting-rooms.component';
+
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
   { path: 'register', component: RegisterComponent },
- 
+  { path: 'meeting-rooms', component: MeetingRoomsComponent },
+
 ];
 
 @NgModule({
